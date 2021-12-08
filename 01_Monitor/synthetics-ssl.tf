4bc5aac0b7ddfba5df45ed0ccd47175bce868901
JJung <jjunghee816@gmail.com>
2021-12-08 23:21:33 +0900

    SSL Certi Monitor

---
+++ 01_Monitor/synthetics-ssl.tf
@@ -0,0 +1,36 @@
+resource "datadog_synthetics_test" "test_ssl" {
+    count   = "${length(var.url_host)}"
+    type    = "api"
+    subtype = "ssl"
+    request_definition {
+        host = "${element(var.url_host, count.index)}"
+        port = 443
+    }
+    assertion {
+        type     = "certificate"
+        operator = "isInMoreThan"
+        target   = 30
+    }
+    assertion {
+        type     = "responseTime"
+        operator = "lessThan"
+        target   = "3000"
+    }
+    locations = ["aws:ap-northeast-2"]
+    options_list {
+        tick_every = 60
+        accept_self_signed = true
+        retry {
+        count    = 2
+        interval = 300
+        }
+        monitor_options {
+        renotify_interval = 100
+        }
+    }
+    name    = "SSL Expires in 30 Days on '${element(var.url_name, count.index)}'"
+    message = "- ***URL*** : [${element(var.url_name, count.index)}](${element(var.url_address, count.index)})\n- ***Port*** : ${(var.ssl_port)}\n- ***Last*** : {{local_time 'last_triggered_at' 'Asia/Seoul'}}{{{{raw}}}}(KST){{{{/raw}}}}\n- ***Notification Channel*** : \n${var.noti_channel}"
+    tags    = ["${var.tag}"]
+    
+    status = "live"
+}

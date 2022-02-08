516ac26eb53b74a3134e6776fd9f7f8197262124
JJung <jjunghee816@gmail.com>
2022-02-09 00:31:55 +0900

    Add location and alert option

--- 01_Monitor/synthetics-ssl.tf
+++ 01_Monitor/synthetics-ssl.tf
@@ -1,40 +1,40 @@
 resource "datadog_synthetics_test" "test_ssl" {
-    count        = "${length(var.url_host)}"
-    type         = "api"
-    subtype      = "ssl"
-    request_definition {
-        host     = "${element(var.url_host, count.index)}"
-        port     = "${var.ssl_port}"
+  count   = length(var.url_host)
+  type    = "api"
+  subtype = "ssl"
+  request_definition {
+    host = element(var.url_host, count.index)
+    port = var.ssl_port
+  }
+  assertion {
+    type     = "certificate"
+    operator = "isInMoreThan"
+    target   = 30
+  }
+  assertion {
+    type     = "responseTime"
+    operator = "lessThan"
+    target   = "3000"
+  }
+  locations = [
+    "aws:ap-northeast-2",
+    "azure:eastus",
+    "##Location you want to monitor##"
+  ]
+  options_list {
+    tick_every         = 60
+    accept_self_signed = true
+    retry {
+      count    = 2
+      interval = 300
     }
-    assertion {
-        type     = "certificate"
-        operator = "isInMoreThan"
-        target   = 30
+    monitor_options {
+      renotify_interval = 100
     }
-    assertion {
-        type     = "responseTime"
-        operator = "lessThan"
-        target   = "3000"
-    }
-    locations = [
-        "aws:ap-northeast-2",
-        "azure:eastus",
-        "##Location you want to monitor##"
-    ]
-    options_list {
-        tick_every = 60
-        accept_self_signed = true
-        retry {
-        count    = 2
-        interval = 300
-        }
-        monitor_options {
-        renotify_interval = 100
-        }
-    }
-    name    = "SSL Expires in 30 Days on '${element(var.url_host, count.index)}'"
-    message = "- ***Domain*** : ${element(var.url_host, count.index)}\n- ***Port*** : ${(var.ssl_port)}\n- ***Last*** : {{local_time 'last_triggered_at' 'Asia/Seoul'}}{{{{raw}}}}(KST){{{{/raw}}}}\n- ***Notification Channel*** : \n${var.noti_channel}"
-    tags    = ["${var.tag}"]
-    
-    status = "live"
+  }
+  name    = "SSL Expires in 30 Days on '${element(var.url_host, count.index)}'"
+  message = "- ***Domain*** : ${element(var.url_host, count.index)}\n- ***Port*** : ${(var.ssl_port)}\n- ***Last*** : {{local_time 'last_triggered_at' 'Asia/Seoul'}}{{{{raw}}}}(KST){{{{/raw}}}}\n- ***Notification Channel*** : \n${var.noti_channel}"
+  tags    = ["${var.tag}"]
+
+  status = "live"
 }

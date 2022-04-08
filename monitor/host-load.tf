82383e1e8fdfbb46961e23121d898ab428acb296
Joselyn Son <SASM-junghee@hwk.net>
2022-04-08 20:23:04 +0900

    modify monitor threshold

--- monitor/host-load.tf
+++ monitor/host-load.tf
@@ -1,7 +1,7 @@
 resource "datadog_monitor" "host-load-1" {
   name                = "System Load over 1 Min {{comparator}} {{threshold}} on '{{host.name}}({{host.ip}})'"
   type                = "metric alert"
-  query               = "max(last_5m):avg:system.load.1{*} by {name,host} >= 10"
+  query               = "max(last_5m):avg:system.load.1{*} by {name,host} > 5"
   notify_no_data      = true
   no_data_timeframe   = 10
   new_group_delay     = 300
@@ -14,7 +14,7 @@ resource "datadog_monitor" "host-load-1" {
 resource "datadog_monitor" "host-load-5" {
   name                = "System Load over 5 Mins {{comparator}} {{threshold}} on '{{host.name}}({{host.ip}})'"
   type                = "metric alert"
-  query               = "max(last_5m):avg:system.load.5{*} by {name,host} >= 10"
+  query               = "max(last_5m):avg:system.load.5{*} by {name,host} > 5"
   notify_no_data      = true
   no_data_timeframe   = 10
   new_group_delay     = 300

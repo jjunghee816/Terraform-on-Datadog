eda06d09ea6b866a300c14e19075cae64c1e27b0
Joselyn Son <SASM-junghee@hwk.net>
2022-04-08 19:25:55 +0900

    initial commit

---
+++ monitor/azure-searchservice.tf
@@ -0,0 +1,25 @@
+resource "datadog_monitor" "azure-search-query" {
+  name                = "Search Service Queries/sec {{comparator}} {{threshold}} on '{{name.name}}'"
+  type                = "metric alert"
+  query               = "avg(last_5m):avg:azure.search_searchservices.search_queries_per_second{*} by {name,resource_group} > 100"
+  notify_no_data      = true
+  no_data_timeframe   = 10
+  new_group_delay     = 300
+  require_full_window = false
+  include_tags        = false
+  message             = "- ***Target*** : {{name.name}}\n- ***Current Value*** : {{value}}\n- ***Last*** : {{local_time 'last_triggered_at' 'Asia/Seoul'}}{{{{raw}}}}(KST){{{{/raw}}}}\n- ***Notification Channel*** : \n${var.noti_channel}"
+  priority            = 2
+}
+
+resource "datadog_monitor" "azure-search-latency" {
+  name                = "Search Service Latency {{comparator}} {{threshold}} on '{{name.name}}'"
+  type                = "metric alert"
+  query               = "min(last_15m):avg:azure.search_searchservices.search_latency{*} by {name,resource_group} > 1"
+  notify_no_data      = true
+  no_data_timeframe   = 0
+  new_group_delay     = 300
+  require_full_window = true
+  include_tags        = false
+  message             = "- ***Target*** : {{name.name}}\n- ***Current Value*** : {{value}}\n- ***Last*** : {{local_time 'last_triggered_at' 'Asia/Seoul'}}{{{{raw}}}}(KST){{{{/raw}}}}\n- ***Notification Channel*** : \n${var.noti_channel}"
+  priority            = 2
+}

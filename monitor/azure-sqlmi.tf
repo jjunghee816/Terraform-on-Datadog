294c6414a094480b2a004fa65de2e89a8ab30687
Joselyn Son <SASM-junghee@hwk.net>
2022-04-08 19:24:53 +0900

    add sqlmi status monitor

--- monitor/azure-sqlmi.tf
+++ monitor/azure-sqlmi.tf
@@ -1,7 +1,33 @@
+resource "datadog_monitor" "azure-sqlmi-status" {
+  name                = "SQL MI Status Degraded on '({{name.name}})'"
+  type                = "metric alert"
+  query               = "max(last_15m):avg:azure.sql_managedinstances.status{*} by {name,type} < 1"
+  notify_no_data      = true
+  no_data_timeframe   = 0
+  evaluation_delay    = 0
+  require_full_window = false
+  include_tags        = false
+  message             = "- ***Target*** : {{name.name}}\n- ***Current Value*** : {{value}}\n- ***Last*** : {{local_time 'last_triggered_at' 'Asia/Seoul'}}{{{{raw}}}}(KST){{{{/raw}}}}\n- ***Notification Channel*** : \n${var.noti_channel}"
+  priority            = 2
+}
+
 resource "datadog_monitor" "azure-sqlmi-cpu" {
   name                = "SQL MI CPU Percent {{comparator}} {{threshold}} on '{{name.name}}'"
   type                = "metric alert"
-  query               = "avg(last_5m):avg:azure.sql_managedinstances.avg_cpu_percent {name:*} by {name} >= 80"
+  query               = "avg(last_5m):avg:azure.sql_managedinstances.avg_cpu_percent {*} by {name} >= 80"
+  notify_no_data      = true
+  no_data_timeframe   = 10
+  evaluation_delay    = 0
+  require_full_window = false
+  include_tags        = false
+  message             = "- ***Target*** : {{name.name}}\n- ***Current Value*** : {{value}}\n- ***Last*** : {{local_time 'last_triggered_at' 'Asia/Seoul'}}{{{{raw}}}}(KST){{{{/raw}}}}\n- ***Notification Channel*** : \n${var.noti_channel}"
+  priority            = 2
+}
+
+resource "datadog_monitor" "azure-sqlmi-storage" {
+  name                = "SQL MI Storage Percent {{comparator}} {{threshold}} on '{{name.name}}'"
+  type                = "metric alert"
+  query               = "max(last_5m):avg:azure.sql_managedinstances.storage_space_used_mb{*} by {name,type} / avg:azure.sql_managedinstances.reserved_storage_mb{*} by {name,type} * 100 >= 60"
   notify_no_data      = true
   no_data_timeframe   = 10
   evaluation_delay    = 0

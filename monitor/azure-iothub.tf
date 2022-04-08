38399cdccbff09027002eca2eba8839d53ef2d28
Joselyn Son <SASM-junghee@hwk.net>
2022-04-08 20:21:09 +0900

    add iothub throttling error monitor

--- monitor/azure-iothub.tf
+++ monitor/azure-iothub.tf
@@ -10,3 +10,16 @@ resource "datadog_monitor" "azure-iothub-message" {
   message             = "- ***Target*** : {{name.name}})\n- ***Current Value*** : {{value}}\n- ***Last*** : {{local_time 'last_triggered_at' 'Asia/Seoul'}}{{{{raw}}}}(KST){{{{/raw}}}}\n- ***Notification Channel*** : \n${var.noti_channel}"
   priority            = 2
 }
+
+resource "datadog_monitor" "azure-iothub-throttling" {
+  name                = "IoT Hub Throttling Errors {{comparator}} {{threshold}} on '{{name.name}}'"
+  type                = "metric alert"
+  query               = "max(last_5m):avg:azure.devices_iothubs.number_of_throttling_errors{*} by {name,resource_group} > 0"
+  notify_no_data      = true
+  no_data_timeframe   = 10
+  evaluation_delay    = 0
+  require_full_window = false
+  include_tags        = false
+  message             = "- ***Target*** : {{name.name}})\n- ***Current Value*** : {{value}}\n- ***Last*** : {{local_time 'last_triggered_at' 'Asia/Seoul'}}{{{{raw}}}}(KST){{{{/raw}}}}\n- ***Notification Channel*** : \n${var.noti_channel}"
+  priority            = 2
+}

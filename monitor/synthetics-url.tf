b6c3695ad19afb7dc3cb4843350ac27edefc896f
Joselyn Son <SASM-junghee@hwk.net>
2022-04-08 19:21:25 +0900

    modify renotify interval

--- monitor/synthetics-url.tf
+++ monitor/synthetics-url.tf
@@ -1,40 +1,40 @@
 resource "datadog_synthetics_test" "synthetics-url" {
-    count   = "${length(var.url_name)}"
-    type    = "api"
-    subtype = "http"
-    request_definition {
-        method = "GET"
-        url    = "${element(var.url_address, count.index)}"
+  count   = length(var.url_name)
+  type    = "api"
+  subtype = "http"
+  request_definition {
+    method = "GET"
+    url    = element(var.url_address, count.index)
+  }
+  assertion {
+    type     = "statusCode"
+    operator = "is"
+    target   = "200"
+  }
+  assertion {
+    type     = "responseTime"
+    operator = "lessThan"
+    target   = "3000"
+  }
+  locations = [
+    "aws:ap-northeast-2",
+    "azure:eastus",
+    "##Location you want to monitor##"
+  ]
+  options_list {
+    tick_every = 60
+    retry {
+      count    = 3
+      interval = 1500
     }
-    assertion {
-        type     = "statusCode"
-        operator = "is"
-        target   = "200"
+    monitor_options {
+      renotify_interval = 120
     }
-    assertion {
-        type     = "responseTime"
-        operator = "lessThan"
-        target   = "3000"
-    }
-    locations = [
-        "aws:ap-northeast-2",
-        "azure:eastus",
-        "##Location you want to monitor##"
-        ]
-    options_list {
-        tick_every = 60
-        retry {
-        count    = 3
-        interval = 1500
-        }
-        monitor_options {
-        renotify_interval = 100
-        }
-        min_failure_duration = 50
-        min_location_failed  = 2
-    }
-    name    = "URL Check on '${element(var.url_name, count.index)}'"
-    message = "- ***URL*** : [${element(var.url_name, count.index)}](${element(var.url_address, count.index)})\n- ***Last*** : {{local_time 'last_triggered_at' 'Asia/Seoul'}}{{{{raw}}}}(KST){{{{/raw}}}}\n- ***Notification Channel*** : \n${var.noti_channel}"
-    tags    = ["${var.tag}"]
-    status  = "live"
+    min_failure_duration = 50
+    min_location_failed  = 2
+  }
+  name    = "URL Check on '${element(var.url_name, count.index)}'"
+  message = "- ***URL*** : [${element(var.url_name, count.index)}](${element(var.url_address, count.index)})\n- ***Last*** : {{local_time 'last_triggered_at' 'Asia/Seoul'}}{{{{raw}}}}(KST){{{{/raw}}}}\n- ***Notification Channel*** : \n${var.noti_channel}"
+  tags    = ["${var.tag}"]
+  status  = "live"
 }

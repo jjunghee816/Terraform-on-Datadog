4ee1eb789a00f5bb70c5c801fea6b6d5bfcf345a
Joselyn Son <SASM-junghee@hwk.net>
2022-04-08 19:32:56 +0900

    add resourcegroup on by value

--- monitor/azure-sqlmi.tf
+++ monitor/azure-sqlmi.tf
@@ -1,7 +1,7 @@
 resource "datadog_monitor" "azure-sqlmi-status" {
   name                = "SQL MI Status Degraded on '({{name.name}})'"
   type                = "metric alert"
-  query               = "max(last_15m):avg:azure.sql_managedinstances.status{*} by {name,type} < 1"
+  query               = "max(last_15m):avg:azure.sql_managedinstances.status{*} by {name,type,resource_group} < 1"
   notify_no_data      = true
   no_data_timeframe   = 0
   evaluation_delay    = 0
@@ -14,7 +14,7 @@ resource "datadog_monitor" "azure-sqlmi-status" {
 resource "datadog_monitor" "azure-sqlmi-cpu" {
   name                = "SQL MI CPU Percent {{comparator}} {{threshold}} on '{{name.name}}'"
   type                = "metric alert"
-  query               = "avg(last_5m):avg:azure.sql_managedinstances.avg_cpu_percent {*} by {name} >= 80"
+  query               = "avg(last_5m):avg:azure.sql_managedinstances.avg_cpu_percent {*} by {name,resource_group} >= 80"
   notify_no_data      = true
   no_data_timeframe   = 10
   evaluation_delay    = 0
@@ -27,7 +27,7 @@ resource "datadog_monitor" "azure-sqlmi-cpu" {
 resource "datadog_monitor" "azure-sqlmi-storage" {
   name                = "SQL MI Storage Percent {{comparator}} {{threshold}} on '{{name.name}}'"
   type                = "metric alert"
-  query               = "max(last_5m):avg:azure.sql_managedinstances.storage_space_used_mb{*} by {name,type} / avg:azure.sql_managedinstances.reserved_storage_mb{*} by {name,type} * 100 >= 60"
+  query               = "max(last_5m):avg:azure.sql_managedinstances.storage_space_used_mb{*} by {name,type,resource_group} / avg:azure.sql_managedinstances.reserved_storage_mb{*} by {name,type,resource_group} * 100 >= 60"
   notify_no_data      = true
   no_data_timeframe   = 10
   evaluation_delay    = 0

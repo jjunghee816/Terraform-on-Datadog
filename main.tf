315d1eeccb92036a61c0b9abbc3f2dc02c7d2795
Joselyn Son <SASM-junghee@hwk.net>
2022-04-08 18:58:59 +0900

    edit indent width

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   required_providers {
-    datadog   = {
+    datadog = {
       source  = "DataDog/datadog"
       version = ">=0.13"
     }
--- variable.tf
+++ variable.tf
@@ -1,37 +1,37 @@
 variable "datadog_api_key" {
-    type        = string
-    sensitive   = true
+  type      = string
+  sensitive = true
 }
 
 variable "datadog_app_key" {
-    type        = string
-    sensitive   = true
+  type      = string
+  sensitive = true
 }
 
 variable "noti_channel" {
-    type        = string
-    sensitive   = true
+  type      = string
+  sensitive = true
 }
 
 variable "url_name" {
-    type        =  list
+  type = list(any)
 }
 
 variable "url_address" {
-    type        = list
-    sensitive   = true
+  type      = list(any)
+  sensitive = true
 }
 
 variable "url_host" {
-    type        = list
-    sensitive   = true
+  type      = list(any)
+  sensitive = true
 }
 
 variable "ssl_port" {
-    type        = number
-    sensitive   = true
+  type      = number
+  sensitive = true
 }
 
 variable "tag" {
-    type        = string
+  type = string
 }

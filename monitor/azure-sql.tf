c12e17a2e2a237b7d2596824d856d61949f9fc78
Joselyn Son <SASM-junghee@hwk.net>
2022-04-11 03:32:37 +0900

    add dtu monitoring composed of query expressions

--- monitor/azure-sql.tf
+++ monitor/azure-sql.tf
@@ -14,7 +14,7 @@ resource "datadog_monitor" "azure-sql-status" {
 resource "datadog_monitor" "azure-sql-connection" {
   name                = "SQL Connection Failed {{comparator}} {{threshold}} on '{{name.name}}'"
   type                = "metric alert"
-  query               = "sum(last_5m):avg:azure.sql_servers_databases.connections_failed{*} by {name,resource_group}.as_count() > 50"
+  query               = "sum(last_5m):avg:azure.sql_servers_databases.connection_failed{*} by {name,resource_group}.as_count() > 50"
   notify_no_data      = true
   no_data_timeframe   = 10
   evaluation_delay    = 0
@@ -25,6 +25,19 @@ resource "datadog_monitor" "azure-sql-connection" {
 }
 
 resource "datadog_monitor" "azure-sql-dtu" {
+  name                = "SQL DTU Usage Percent {{comparator}} {{threshold}} on '{{name.name}}'"
+  type                = "metric alert"
+  query               = "avg(last_15m):( avg:azure.sql_servers_databases.dtu_used{*} by {server_name,resource_group}.as_rate() / avg:azure.sql_servers_databases.dtu_limit{*} by {server_name,resource_group}.as_rate() ) * 100 > 90"
+  notify_no_data      = true
+  no_data_timeframe   = 0
+  evaluation_delay    = 0
+  require_full_window = false
+  include_tags        = false
+  message             = "- ***Target*** : {{name.name}}\n- ***Current Value*** : {{value}}\n- ***Last*** : {{local_time 'last_triggered_at' 'Asia/Seoul'}}{{{{raw}}}}(KST){{{{/raw}}}}\n- ***Notification Channel*** : \n${var.noti_channel}"
+  priority            = 2
+}
+
+resource "datadog_monitor" "azure-sql-dtu-divide" {
   name                = "SQL DTU Usage Percent {{comparator}} {{threshold}} on '{{name.name}}'"
   type                = "metric alert"
   query               = "avg(last_15m):( avg:azure.sql_servers_databases.dtu_used{*} by {server_name,resource_group}.as_rate() / avg:azure.sql_servers_databases.dtu_limit{*} by {server_name,resource_group}.as_rate() ) * 100 > 90"

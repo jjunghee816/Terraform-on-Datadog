eb372a7dd12e2a0cb06c6ba8524a677929a0011a
Joselyn Son <SASM-junghee@hwk.net>
2022-04-11 03:34:12 +0900

    add iothub status monitor

--- monitor/azure-iothub.tf
+++ monitor/azure-iothub.tf
@@ -1,3 +1,16 @@
+resource "datadog_monitor" "azure-iothub-status" {
+  name                = "IoT Hub Degraded on '{{name.name}}'"
+  type                = "metric alert"
+  query               = "min(last_5m):avg:azure.devices_iothubs.status{*} by {name,resource_group} < 1"
+  notify_no_data      = true
+  no_data_timeframe   = 10
+  evaluation_delay    = 0
+  require_full_window = false
+  include_tags        = false
+  message             = "- ***Target*** : {{name.name}}\n- ***Current Value*** : {{value}}\n- ***Last*** : {{local_time 'last_triggered_at' 'Asia/Seoul'}}{{{{raw}}}}(KST){{{{/raw}}}}\n- ***Notification Channel*** : \n${var.noti_channel}"
+  priority            = 2
+}
+
 resource "datadog_monitor" "azure-iothub-message" {
   name                = "IoT Hub Total Messages {{comparator}} {{threshold}} on '{{name.name}}'"
   type                = "metric alert"

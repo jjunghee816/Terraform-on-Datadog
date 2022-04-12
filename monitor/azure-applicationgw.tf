265da067b5eb98510395270757f8754b8c5a5b4b
Joselyn Son <SASM-junghee@hwk.net>
2022-04-13 02:36:27 +0900

    add message to check backendpool on AAG status

--- monitor/azure-applicationgw.tf
+++ monitor/azure-applicationgw.tf
@@ -7,6 +7,6 @@ resource "datadog_monitor" "azure-agw-status" {
   evaluation_delay    = 0
   require_full_window = false
   include_tags        = false
-  message             = "- ***Target*** : {{name.name}}\n- ***Current Value*** : {{value}}\n- ***Last*** : {{local_time 'last_triggered_at' 'Asia/Seoul'}}{{{{raw}}}}(KST){{{{/raw}}}}\n- ***Notification Channel*** : \n${var.noti_channel}"
+  message             = "- ***Target*** : {{name.name}}\n- ***Backend Pool*** : {{backendsettingspool.name}}\n- ***Current Value*** : {{value}}\n- ***Last*** : {{local_time 'last_triggered_at' 'Asia/Seoul'}}{{{{raw}}}}(KST){{{{/raw}}}}\n- ***Notification Channel*** : \n${var.noti_channel}"
   priority            = 2
 }

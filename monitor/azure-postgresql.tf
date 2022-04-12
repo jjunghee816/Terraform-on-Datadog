1ee114000aedc7511b1a0f4921d2344ccc185d16
Joselyn Son <SASM-junghee@hwk.net>
2022-04-13 02:37:11 +0900

    add postgresql memory monitor

--- monitor/azure-postgresql.tf
+++ monitor/azure-postgresql.tf
@@ -1,7 +1,20 @@
 resource "datadog_monitor" "azure-postgresql-cpu" {
   name                = "PostgreSQL CPU Percent {{comparator}} {{threshold}} on '{{name.name}}'"
   type                = "metric alert"
-  query               = "avg(last_5m):avg:azure.dbforpostgresql_servers.cpu_percent{*} by {name,resource_group} >= 80"
+  query               = "avg(last_5m):avg:azure.dbforpostgresql_servers.cpu_percent{*} by {name,resource_group} > 80"
+  notify_no_data      = true
+  no_data_timeframe   = 10
+  evaluation_delay    = 0
+  require_full_window = false
+  include_tags        = false
+  message             = "- ***Target*** : {{name.name}}\n- ***Current Value*** : {{value}}\n- ***Last*** : {{local_time 'last_triggered_at' 'Asia/Seoul'}}{{{{raw}}}}(KST){{{{/raw}}}}\n- ***Notification Channel*** : \n${var.noti_channel}"
+  priority            = 2
+}
+
+resource "datadog_monitor" "azure-postgresql-memory" {
+  name                = "PostgreSQL Memory Percent {{comparator}} {{threshold}} on '{{name.name}}'"
+  type                = "metric alert"
+  query               = "max(last_5m):avg:azure.dbforpostgresql_servers.memory_percent{*} by {name,resource_group} > 80"
   notify_no_data      = true
   no_data_timeframe   = 10
   evaluation_delay    = 0

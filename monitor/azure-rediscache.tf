97821a57711c16a30789301f6584a56a29f0327c
Joselyn Son <SASM-junghee@hwk.net>
2022-04-12 21:27:54 +0900

    add azure redis cache monitor

---
+++ monitor/azure-rediscache.tf
@@ -0,0 +1,51 @@
+resource "datadog_monitor" "azure-redis-status" {
+  name                = "Redis Cache Status Degraded on '{{name.name}}'"
+  type                = "metric alert"
+  query               = "max(last_5m):avg:azure.cache_redis.status{*} by {name,resource_group} < 1"
+  notify_no_data      = true
+  no_data_timeframe   = 10
+  evaluation_delay    = 0
+  require_full_window = false
+  include_tags        = false
+  message             = "- ***Target*** : {{name.name}}\n- ***Current Value*** : {{value}}\n- ***Last*** : {{local_time 'last_triggered_at' 'Asia/Seoul'}}{{{{raw}}}}(KST){{{{/raw}}}}\n- ***Notification Channel*** : \n${var.noti_channel}"
+  priority            = 2
+}
+
+resource "datadog_monitor" "azure-redis-load" {
+  name                = "Redis Cache Load {{comparator}} {{threshold}} on '{{name.name}}'"
+  type                = "metric alert"
+  query               = "avg(last_5m):avg:azure.cache_redis.server_load{*} by {name,resource_group} > 80"
+  notify_no_data      = true
+  no_data_timeframe   = 10
+  new_group_delay     = 300
+  require_full_window = false
+  include_tags        = false
+  message             = "- ***Target*** : {{name.name}}\n- ***Current Value*** : {{value}}\n- ***Last*** : {{local_time 'last_triggered_at' 'Asia/Seoul'}}{{{{raw}}}}(KST){{{{/raw}}}}\n- ***Notification Channel*** : \n${var.noti_channel}"
+  priority            = 2
+}
+
+resource "datadog_monitor" "azure-redis-load-instance" {
+  name                = "Redis Cache Instance Load {{comparator}} {{threshold}} on '{{name.name}}'"
+  type                = "metric alert"
+  query               = "avg(last_5m):avg:azure.cache_redis.server_load_instance_based{*} by {name,port,resource_group} > 80"
+  notify_no_data      = true
+  no_data_timeframe   = 10
+  new_group_delay     = 300
+  require_full_window = false
+  include_tags        = false
+  message             = "- ***Target*** : {{name.name}}\n- ***Port*** : {{port.name}}\n- ***Current Value*** : {{value}}\n- ***Last*** : {{local_time 'last_triggered_at' 'Asia/Seoul'}}{{{{raw}}}}(KST){{{{/raw}}}}\n- ***Notification Channel*** : \n${var.noti_channel}"
+  priority            = 2
+}
+
+resource "datadog_monitor" "azure-redis-error" {
+  name                = "Redis Cache Errors {{comparator}} {{threshold}} on '{{name.name}}'"
+  type                = "metric alert"
+  query               = "avg(last_5m):avg:azure.cache_redis.errors{*} by {name,resource_group} > 0"
+  notify_no_data      = true
+  no_data_timeframe   = 10
+  new_group_delay     = 300
+  require_full_window = false
+  include_tags        = false
+  message             = "- ***Target*** : {{name.name}}\n- ***Current Value*** : {{value}}\n- ***Last*** : {{local_time 'last_triggered_at' 'Asia/Seoul'}}{{{{raw}}}}(KST){{{{/raw}}}}\n- ***Notification Channel*** : \n${var.noti_channel}"
+  priority            = 2
+}

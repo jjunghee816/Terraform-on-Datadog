655a8405ec2ad669c561d73c332706da215d51fb
Joselyn Son <SASM-junghee@hwk.net>
2022-04-13 21:12:14 +0900

    add ssl monitor using for_each loop

--- monitor/synthetics-ssl.tf
+++ monitor/synthetics-ssl.tf
@@ -1,4 +1,4 @@
-resource "datadog_synthetics_test" "synthetics-ssl" {
+resource "datadog_synthetics_test" "synthetics-ssl-count" {
   count   = length(var.url_host)
   type    = "api"
   subtype = "ssl"
@@ -37,4 +37,45 @@ resource "datadog_synthetics_test" "synthetics-ssl" {
   tags    = ["${var.tag}"]
 
   status = "live"
+}
+
+resource "datadog_synthetics_test" "synthetics-ssl-foreach" {
+  for_each = var.url
+  type     = "api"
+  subtype  = "ssl"
+  request_definition {
+    host = substr(replace(each.value, "##string##", ""), 8, -1)
+    port = var.ssl_port
+  }
+  assertion {
+    type     = "certificate"
+    operator = "isInMoreThan"
+    target   = 30
+  }
+  assertion {
+    type     = "responseTime"
+    operator = "lessThan"
+    target   = "3000"
+  }
+  locations = [
+    "aws:ap-northeast-2",
+    "azure:eastus"
+  ]
+  options_list {
+    tick_every         = 86400
+    accept_self_signed = true
+    retry {
+      count    = 3
+      interval = 1500
+    }
+    monitor_options {
+      renotify_interval = 1440
+    }
+    min_failure_duration = 50
+    min_location_failed  = 2
+  }
+  name    = "SSL Expires in 30 Days on '${each.key}'"
+  message = "- ***Domain*** : ${substr(replace(each.value, "##string##", ""), 8, -1)}\n- ***Port*** : ${(var.ssl_port)}\n- ***Last*** : {{local_time 'last_triggered_at' 'Asia/Seoul'}}{{{{raw}}}}(KST){{{{/raw}}}}\n- ***Notification Channel*** : \n${var.noti_channel}"
+  tags    = ["${var.tag}"]
+  status  = "live"
 }

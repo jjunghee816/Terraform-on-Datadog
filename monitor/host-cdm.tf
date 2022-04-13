1d62129ee03ac39887ae2716a78262443497bbc6
Joselyn Son <SASM-junghee@hwk.net>
2022-04-13 15:27:39 +0900

    add memory and disk usage forecast monitor

--- monitor/host-cdm.tf
+++ monitor/host-cdm.tf
@@ -24,27 +24,43 @@ resource "datadog_monitor" "host-memory" {
   priority            = 2
 }
 
-resource "datadog_monitor" "host-disk-device" {
+resource "datadog_monitor" "host-memory-forecast" {
+  name                = "[Forecast] Memory Percent {{comparator}} {{threshold}} on '{{host.name}}({{host.ip}})' within the next week"
+  type                = "metric alert"
+  query               = "max(next_1w):forecast(( avg:system.mem.total{*} by {name,host} - avg:system.mem.usable{*} by {name,host} ) / avg:system.mem.total{*} by {name,host} * 100, 'seasonal', 1, model='default', interval='60m', history='1w', seasonality='weekly', timezone='Asia/Seoul') > 93"
+  notify_no_data      = false
+  no_data_timeframe   = 10
+  new_group_delay     = 60
+  require_full_window = false
+  include_tags        = false
+  message             = "- ***Target*** : {{host.name}}({{host.ip}})\n- ***Forecast Value*** : {{value}}\n- ***Last*** : {{local_time 'last_triggered_at' 'Asia/Seoul'}}{{{{raw}}}}(KST){{{{/raw}}}}\n- ***Notification Channel*** : \n${var.noti_channel}"
+  priority            = 2
+}
+
+resource "datadog_monitor" "host-disk" {
   name                = "Disk '{{device.name}}' Percent {{comparator}} {{threshold}} on '{{host.name}}({{host.ip}})'"
   type                = "metric alert"
   query               = "avg(last_3m):avg:system.disk.in_use{device:/dev/s* OR device:*: OR device://*} by {name,host,device} * 100 >= 80"
   notify_no_data      = true
   no_data_timeframe   = 10
+  new_group_delay     = 60
   require_full_window = false
   include_tags        = false
-  message             = "- ***Target*** : {{host.name}}({{host.ip}})\n- ***Disk*** : {{device.name}}\n- ***Current Value*** : {{value}}\n- ***Last*** : {{last_triggered_at}}{{{{raw}}}}(UTC){{{{/raw}}}}\n- ***Notification Channel*** : \n${var.noti_channel}"
+  message             = "- ***Target*** : {{host.name}}({{host.ip}})\n- ***Disk*** : {{device.name}}\n- ***Current Value*** : {{value}}\n- ***Last*** : {{last_triggered_at}}{{{{raw}}}}(UTC){{{{/raw}}}}\n- ***Notification Channel*** : \n${var.noti_channel_exclude_noc}"
   priority            = 2
 }
 
 resource "datadog_monitor" "host-disk-forecast" {
-  name              = "Disk '{{device.name}}' Percent {{comparator}} {{threshold}} on '{{host.name}}({{host.ip}})' within the next 1 week"
-  type              = "metric alert"
-  query             = "max(next_1w):forecast(system.disk.in_use{device:/dev/s* OR device:*: OR device://*} by {name} * 100, 'seasonal', 1, interval='60m', seasonality='weekly', timezone='asia/seoul') >= 85"
-  notify_no_data    = true
-  no_data_timeframe = 10
-  include_tags      = false
-  message           = "- ***Target*** : {{host.name}}({{host.ip}})\n- ***Disk*** : {{device.name}}\n- ***Current Value*** : {{value}}\n- ***Last*** : {{local_time 'last_triggered_at' 'Asia/Seoul'}}{{{{raw}}}}(KST){{{{/raw}}}}\n- ***Notification Channel*** : \n${var.noti_channel}"
-  priority          = 2
+  name                = "[Forecast] Disk '{{device.name}}' Percent {{comparator}} {{threshold}} on '{{host.name}}({{host.ip}})' within the next week"
+  type                = "metric alert"
+  query               = "max(next_1w):forecast(system.disk.in_use{device:/dev/s* OR device:*: OR device://*} by {name} * 100, 'seasonal', 1, model='default', interval='60m', history='1w', seasonality='weekly', timezone='Asia/Seoul') > 85"
+  notify_no_data      = false
+  no_data_timeframe   = 10
+  new_group_delay     = 60
+  require_full_window = false
+  include_tags        = false
+  message             = "- ***Target*** : {{host.name}}({{host.ip}})\n- ***Disk*** : {{device.name}}\n- ***Current Value*** : {{value}}\n- ***Last*** : {{local_time 'last_triggered_at' 'Asia/Seoul'}}{{{{raw}}}}(KST){{{{/raw}}}}\n- ***Notification Channel*** : \n${var.noti_channel_exclude_noc}"
+  priority            = 2
 }
 
 resource "datadog_monitor" "azure-disk-queue" {

c7340f19bb148190129966a298f8374fff9baa27
Joselyn Son <SASM-junghee@hwk.net>
2022-04-13 20:57:03 +0900

    add aks monitor

---
+++ monitor/azure-aks.tf
@@ -0,0 +1,51 @@
+resource "datadog_monitor" "azure-aks-status" {
+  name                = "AKS Node Status Degraded on '{{name.name}}'"
+  type                = "metric alert"
+  query               = "max(last_5m):avg:azure.containerservice_managedclusters.kube_node_status_condition{status:true} by {name,node,status,status2,resource_group} < 1"
+  notify_no_data      = true
+  no_data_timeframe   = 10
+  evaluation_delay    = 0
+  require_full_window = false
+  include_tags        = false
+  message             = "- ***Target*** : {{name.name}}\n- ***Node*** : {{node.name}}\n- ***Current Value*** : {{value}}\n- ***Last*** : {{local_time 'last_triggered_at' 'Asia/Seoul'}}{{{{raw}}}}(KST){{{{/raw}}}}\n- ***Notification Channel*** : \n${var.noti_channel}"
+  priority            = 2
+}
+
+resource "datadog_monitor" "azure-aks-cpu" {
+  name                = "AKS CPU Usage Percent {{comparator}} {{threshold}} on '{{name.name}}'"
+  type                = "metric alert"
+  query               = "avg(last_5m):azure.containerservice_managedclusters.node_cpu_usage_percentage{*} by {name,nodepool,resource_group} > 90"
+  notify_no_data      = true
+  no_data_timeframe   = 10
+  evaluation_delay    = 0
+  require_full_window = false
+  include_tags        = false
+  message             = "- ***Target*** : {{name.name}}\n- ***Node Pool*** : {{nodepool.name}}\n- ***Node*** : {{node.name}}\n- ***Disk*** : {{device.name}}\n- ***Current Value*** : {{value}}\n- ***Last*** : {{local_time 'last_triggered_at' 'Asia/Seoul'}}{{{{raw}}}}(KST){{{{/raw}}}}\n- ***Notification Channel*** : \n${var.noti_channel}"
+  priority            = 2
+}
+
+resource "datadog_monitor" "azure-aks-memory" {
+  name                = "AKS Memory Usage Percent {{comparator}} {{threshold}} on '{{name.name}}'"
+  type                = "metric alert"
+  query               = "avg(last_5m):azure.containerservice_managedclusters.node_memory_working_set_percentage{*} by {name,nodepool,resource_group} > 90"
+  notify_no_data      = true
+  no_data_timeframe   = 10
+  evaluation_delay    = 0
+  require_full_window = false
+  include_tags        = false
+  message             = "- ***Target*** : {{name.name}}\n- ***Node Pool*** : {{nodepool.name}}\n- ***Node*** : {{node.name}}\n- ***Disk*** : {{device.name}}\n- ***Current Value*** : {{value}}\n- ***Last*** : {{local_time 'last_triggered_at' 'Asia/Seoul'}}{{{{raw}}}}(KST){{{{/raw}}}}\n- ***Notification Channel*** : \n${var.noti_channel}"
+  priority            = 2
+}
+
+resource "datadog_monitor" "azure-aks-disk" {
+  name                = "AKS Disk Usage Percent {{comparator}} {{threshold}} on '{{name.name}}'"
+  type                = "metric alert"
+  query               = "avg(last_5m):azure.containerservice_managedclusters.node_disk_usage_percentage{*} by {name,nodepool,resource_group} > 90"
+  notify_no_data      = true
+  no_data_timeframe   = 10
+  evaluation_delay    = 0
+  require_full_window = false
+  include_tags        = false
+  message             = "- ***Target*** : {{name.name}}\n- ***Node Pool*** : {{nodepool.name}}\n- ***Node*** : {{node.name}}\n- ***Disk*** : {{device.name}}\n- ***Current Value*** : {{value}}\n- ***Last*** : {{local_time 'last_triggered_at' 'Asia/Seoul'}}{{{{raw}}}}(KST){{{{/raw}}}}\n- ***Notification Channel*** : \n${var.noti_channel}"
+  priority            = 2
+}
